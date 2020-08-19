156ff0a2eac602c3e6f1cc6a29aa2f133ec3a4f0
Ele-W <68557244+Ele-W@users.noreply.github.com>
2020-08-20 01:10:35 +0800

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,14 @@
 # -OCTO-
+//乘法表
+public class T04 {
+    public static void main(String[] args) {
+        int i,j,c;
+        for (i=1;i<=9;i++) {
+            for (j = 1; j <= i; j++) {
+                c=i*j;
+                System.out.print("  "+i+"*"+j+"="+c);
+            }
+            System.out.print("\n");
+        }
+    }
+}
